b1ad181f055a990ec9919f36374aa3c6213b22b7
Mattia Righetti <matt95.righetti@gmail.com>
2020-11-13 15:45:10 +0100

    Updated DB methods to use manual transactions

--- database/db.go
+++ database/db.go
@@ -1,12 +1,12 @@
 package database
 
 import (
-	"fmt"
 	"log"
 
 	badger "github.com/dgraph-io/badger/v2"
 )
 
+// DB instance of the BadgerDB
 var DB *badger.DB
 
 // DbInit executes a function that initiates and opens BadgerDB
@@ -20,57 +20,66 @@ func DbInit() {
 
 // Write executes a function that will write (key, value) to BadgerDB
 func Write(key string, value string) error {
-	return DB.Update(func(txn *badger.Txn) error {
-		err := txn.Set([]byte(key), []byte(value))
+	txn := DB.NewTransaction(true)
+	defer txn.Discard()
+
+	if err := txn.Set([]byte(key), []byte(value)); err != nil {
 		return err
-	})
+	}
+
+	if err := txn.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Get executes a function that will print the value found with key in BadgerDB
+func Get(key string) ([]byte, error) {
+	txn := DB.NewTransaction(true)
+	defer txn.Discard()
+
+	item, err := txn.Get([]byte(key))
+	if err != nil {
+		return nil, err
+	}
+
+	val, err := item.ValueCopy(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return val, nil
 }
 
 // IsPresent executes a function that will return a boolean value indicating wether a key is present or not in the BadgerDB
 func IsPresent(key string) bool {
-	if err := DB.View(func(txn *badger.Txn) error {
-		_, err := txn.Get([]byte(key))
-		if err != nil {
-			return err
-		}
+	txn := DB.NewTransaction(true)
+	defer txn.Discard()
 
-		return nil
-	}); err != nil {
+	_, err := txn.Get([]byte(key))
+	if err != nil {
 		return false
 	}
 
 	return true
 }
 
-// Get executes a function that will print the value found with key in BadgerDB
-func Get(key string) error {
-	return DB.View(func(txn *badger.Txn) error {
-		item, err := txn.Get([]byte(key))
-		if err != nil {
-			return err
-		}
-
-		val, err := item.ValueCopy(nil)
-		if err != nil {
-			return err
-		}
-
-		fmt.Println(string(val))
-
-		return nil
-	})
-}
-
 // Delete executes a function that will delete, if present, the value with specified key in BadgerDB
 func Delete(key string) error {
-	return DB.Update(func(txn *badger.Txn) error {
-		err := txn.Delete([]byte(key))
-		if err != nil {
-			return err
-		}
+	txn := DB.NewTransaction(true)
+	defer txn.Discard()
 
-		return nil
-	})
+	err := txn.Delete([]byte(key))
+	if err != nil {
+		return err
+	}
+
+	if err := txn.Commit(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // CloseDb closes BadgerDB
