7729a72fe50c7334c756a9d3980e923fdded7a9e
Mattia Righetti <matt95.righetti@gmail.com>
2020-11-13 19:53:25 +0100

    Pretty format GET

--- cmd/get.go
+++ cmd/get.go
@@ -7,22 +7,33 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var getCmd = &cobra.Command{
-	Use:   "get [PASSWORD_ID]",
-	Short: "Prints the password",
-	Long:  "examples here...",
-	Run: func(cmd *cobra.Command, args []string) {
-		if db.IsPresent(args[0]) {
-			err := db.Get(args[0])
-			if err != nil {
-				fmt.Printf("Could not get value with key %s\n", args[0])
+var (
+	passwdOnly bool
+
+	getCmd = &cobra.Command{
+		Use:   "get [PASSWORD_ID]",
+		Short: "Prints the password",
+		Long:  "examples here...",
+		Run: func(cmd *cobra.Command, args []string) {
+			if db.IsPresent(args[0]) {
+				val, err := db.Get(args[0])
+				if err != nil {
+					fmt.Printf("Could not get value with key %s\n", args[0])
+				}
+
+				if passwdOnly {
+					fmt.Printf("%s\n", string(val))
+				} else {
+					fmt.Printf("ID: %s -> Password: %s\n", args[0], string(val))
+				}
+			} else {
+				fmt.Printf("No password stored with key %s\n", args[0])
 			}
-		} else {
-			fmt.Printf("No password stored with key %s\n", args[0])
-		}
-	},
-}
+		},
+	}
+)
 
 func init() {
 	rootCmd.AddCommand(getCmd)
+	getCmd.Flags().BoolVarP(&passwdOnly, "all", "a", true, "Flag to indicated to print passwork only or id and password")
 }
