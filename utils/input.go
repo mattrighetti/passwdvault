8a30a13707df452663810dc1de6d361276a8abbe
Mattia Righetti <matt95.righetti@gmail.com>
2020-11-15 02:37:16 +0100

    Added docs to public utils methods

--- utils/input.go
+++ utils/input.go
@@ -7,11 +7,13 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+// ReadInputString reads a string from Stdin and puts that value in s
 func ReadInputString(displayText string, s *string) {
 	fmt.Print(displayText)
 	fmt.Scanf("%s", s)
 }
 
+// ReadBool asks user to input y/n and returns a bool
 func ReadBool(displayText string) bool {
 	var res string
 	fmt.Printf("%s [y/n]: ", displayText)
@@ -23,9 +25,11 @@ func ReadBool(displayText string) bool {
 	return true
 }
 
+// ReadInputStringHideInput reads input from user hiding Stdin chars
 func ReadInputStringHideInput(displayText string) ([]byte, error) {
 	fmt.Print(displayText)
 	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	fmt.Println()
 	if err != nil {
 		return nil, err
 	}
