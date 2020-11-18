5b9f4cd179dc614b54717640ec4f3161e51399ae
serant <seran.thirugnanam@gmail.com>
2020-11-18 00:36:43 -0500

    Updated prompts on init to do input validation

--- cmd/init.go
+++ cmd/init.go
@@ -22,10 +22,13 @@ var (
 		PreRun: func(cmd *cobra.Command, args []string) {
 			exists := utils.FileExists(configuration.ConfigFilePath)
 			if exists {
-				var res string
-				fmt.Print("A configuration file already exist, would you like to overwrite it? [y/n]: ")
-				fmt.Scanf("%s", &res)
-				if res != "y" {
+				overwrite, err := utils.ReadBool("A configuration file already exist, would you like to overwrite it?")
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if overwrite {
 					os.Exit(0)
 				}
 			}
@@ -34,8 +37,11 @@ var (
 			var config configuration.Configuration
 			var masterkey []byte = nil
 
-			utils.ReadInputString("Your name: ", &config.User.Name)
-			utils.ReadInputString("Your email: ", &config.User.Email)
+			unamePattern := `^[a-zA-Z0-9]+(?:[-_][a-zA-Z0-9]+)*$`
+			emailPattern := "^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+
+			utils.ReadValidatedInputString("Your name: ", &config.User.Name, unamePattern, 8, 20)
+			utils.ReadValidatedInputString("Your email: ", &config.User.Email, emailPattern, 3, 254)
 
 			dir, err := os.Getwd()
 			if err != nil {
@@ -45,7 +51,11 @@ var (
 			config.Database.Path = dir
 			config.Database.Name = ".passwddatabase"
 
-			confirm := utils.ReadBool("Would you like to encrypt the database?")
+			confirm, err := utils.ReadBool("Would you like to encrypt the database?")
+			if err != nil {
+				log.Fatal(err)
+			}
+
 			if confirm {
 				config.Database.Encrypted = true
 
@@ -58,7 +68,12 @@ var (
 				config.Database.MasterKey.Length = int8(len(masterkey))
 				viper.Set("masterkey", string(masterkey))
 
-				confirm := utils.ReadBool("Would you like to store the masterkey somewhere in your system in order to avoid writing everytime?")
+				confirm, err := utils.ReadBool("Would you like to store the masterkey somewhere in your system in order to avoid writing everytime?")
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
 				if confirm {
 					fmt.Print("Insert path to file: ")
 					fmt.Scanf("%s", &config.Database.MasterKey.FromFilePath)
--- utils/input.go
+++ utils/input.go
@@ -11,27 +11,35 @@ import (
 )
 
 // ReadInputString reads a string from Stdin and puts that value in s
-func ReadInputString(displayText string, s *string) {
+func ReadInputString(displayText string, s *[]byte) {
 	fmt.Print(displayText)
 	in := bufio.NewScanner(os.Stdin)
 	in.Scan()
-	*s = in.Text()
+	*s = in.Bytes()
 }
 
 // ReadValidatedInputString reads lines from Stdin until a pattern is achieved, then stores in s
-func ReadValidatedInputString(displayText string, s *string, pattern string) error {
-	var temp string
+func ReadValidatedInputString(displayText string, s *string, pattern string, minLength int, maxLength int) error {
+	var buff []byte
+
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		return err
+	}
 
 	for {
-		ReadInputString(displayText, &temp)
-		matched, err := regexp.Match(pattern, []byte(temp))
+		ReadInputString(displayText, &buff)
+		matched := re.Match(buff)
 
-		if err != nil {
-			return err
-		} else if !matched {
-			fmt.Println("Invalid value: ", temp)
-		} else {
-			*s = temp
+		switch {
+		case !matched:
+			fmt.Printf("Invalid value: %s\n", buff)
+		case len(buff) < minLength:
+			fmt.Printf("Input must be at least %d character(s) long\n", minLength)
+		case len(buff) > maxLength:
+			fmt.Printf("Input cannot be longer than %d character(s)\n", maxLength)
+		default:
+			*s = string(buff)
 			return nil
 		}
 	}
@@ -40,13 +48,20 @@ func ReadValidatedInputString(displayText string, s *string, pattern string) err
 // ReadBool asks user to input y/n and returns a bool
 func ReadBool(displayText string) (bool, error) {
 	var res string
-	fmt.Printf("%s [y/n]: ", displayText)
-	fmt.Scanf("%s", &res)
-	if res != "y" {
-		return false, nil
-	}
+	text := fmt.Sprintf("%s [y/n]: ", displayText)
 
-	return true, nil
+	err := ReadValidatedInputString(text, &res, "[yn]", 1, 1)
+
+	switch {
+	case err != nil:
+		return false, err
+	case res == "y":
+		return true, nil
+	case res == "n":
+		return false, nil
+	default:
+		return false, fmt.Errorf("Cannot convert input to bool: %s", res)
+	}
 }
 
 // ReadInputStringHideInput reads input from user hiding Stdin chars
